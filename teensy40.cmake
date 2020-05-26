24e7b4df03d6d902502ff74f8e12caa94dd5d8f1
Ermal Dreshaj <ermal@apple.com>
2020-05-25 22:02:47 -0700

    added defines for the MCU & speed

--- teensy40.cmake
+++ teensy40.cmake
@@ -24,9 +24,9 @@ message("Linker script: ${LINKER_SCRIPT}")
 
 set(OBJECT_GEN_FLAGS "-mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -MMD -nostartfiles")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OBJECT_GEN_FLAGS}" CACHE INTERNAL "C Compiler options")
-set(CMAKE_CXX_FLAGS "-fno-unwind-tables -fno-exceptions -fpermissive -fno-rtti -fno-threadsafe-statics -felide-constructors -Wno-error=narrowing ${CMAKE_CXX_FLAGS} ${OBJECT_GEN_FLAGS}" CACHE INTERNAL "C++ Compiler options")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${OBJECT_GEN_FLAGS}" CACHE INTERNAL "ASM Compiler options")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OBJECT_GEN_FLAGS} -D__IMXRT1062__ -DF_CPU=600000000" CACHE INTERNAL "C Compiler options")
+set(CMAKE_CXX_FLAGS "-fno-unwind-tables -fno-exceptions -fpermissive -fno-rtti -fno-threadsafe-statics -felide-constructors -Wno-error=narrowing ${CMAKE_CXX_FLAGS} ${OBJECT_GEN_FLAGS} -D__IMXRT1062__ -DF_CPU=600000000" CACHE INTERNAL "C++ Compiler options")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${OBJECT_GEN_FLAGS} -D__IMXRT1062__ -DF_CPU=600000000" CACHE INTERNAL "ASM Compiler options")
 
 # Linker flags
 if(PRINT_SIZE_INFO_AFTER_LINK)
